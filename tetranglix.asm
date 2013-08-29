591012e6a8e1682157d29eea747b04d07fb517c9
Shikhin Sethi <draumr.shikhin@gmail.com>
2013-08-30 01:28:13 +0530

    Cut down on bytes.

--- tetranglix.asm
+++ tetranglix.asm
@@ -2,18 +2,20 @@
 BITS 16
 ORG 0x7C00
 
-; Contrary to widespread information, the byte at 0x500 is also used.
-BSS           EQU 0x504
-BSS_SIZE      EQU 34 ; why 34?
+BSS             EQU 0x504     ; The byte at 0x500 is also used, so align on next dword bound.
+BSS_SIZE        EQU 34        ; 16 for CUR_TETRAMINO, 16 for FLIP_TETRAMINO, 2 for (x, y) coordinate pair.
 
-CUR_TETRAMINO EQU BSS ; 16 bytes
+CUR_TETRAMINO   EQU BSS       ; 16 bytes.
+FLIP_TETRAMINO  EQU BSS + 16  ; 16 bytes.
+OFFSET          EQU BSS + 32  ; 2 bytes.
 
 CPU 186
 
+; Entry point.
 ;     dl    -> the drive number.
 ;     cs:ip -> linear address 0x7C00.
 start:
-    jmp 0x0000:.flush_CS                    ; Some BIOS' may load us at 0x0000:0x7C00, while others at 0x07C0:0x0000. Let's just make this uniform.
+    jmp 0x0000:.flush_CS                    ; Some BIOS' may load us at 0x0000:0x7C00, while others at 0x07C0:0x0000.
 
     ; Generic error procedure.
     .error:
@@ -66,59 +68,61 @@ start:
     mov cx, 80*25
     mov ax, 0x0F20
     rep stosw
-    
+
     jmp $
 
-;    al=which tetramino to load
+; Load a tetramino to CUR_TETRAMINO, from the compressed bitmap format.
+;     al -> tetramino index.
 load_tetramino:
     pusha
-    
-    ; Set the load address for tetramino (in bitmap=
+
+    ; Get the address of the tetramino (in bitmap format).
     xor ah, ah
     shl al, 2
     add ax, tetraminos
-    
-    ; Load tetramino bitmap in ax
+
+    ; Load tetramino bitmap in ax.
     mov bx, ax
-    mov word ax, [bx]
-    
-    ; Convert from bitmap to array
+    mov ax, [bx]
+
+    ; Convert from bitmap to array.
     mov dx, 0x8000
     mov bx, CUR_TETRAMINO
-    mov cx, 0x10
-.loop:
-    mov si, ax
-    and ax, dx
-    jz .zero
+
+    .loop:
+        test ax, dx
+        jz .zero
     
-    .one:
-        mov byte [bx], 1
-        jmp .loopend
-    .zero:
-        mov byte [bx], 0
-    .loopend:
+            mov byte [bx], 1
+            jmp .loop_end
+    
+        .zero:
+            mov byte [bx], 0
+    
+    .loop_end:
         inc bx
-        mov ax, si
+
         shr dx, 1
-        loop .loop
+        jnz .loop
     
     popa
     ret
-    
+
+; All tetraminos in bitmap format.
 tetraminos:
-    dw 0b0000111100000000 ; I
-    dw 0b0000111000100000 ; J
-    dw 0b0000001011100000 ; L
-    dw 0b0000011001100000 ; O
-    dw 0b0000001101100000 ; S
-    dw 0b0000111001000000 ; T
-    dw 0b0000011000110000 ; Z
+    dw 0b0000111100000000   ; I
+    dw 0b0000111000100000   ; J
+    dw 0b0000001011100000   ; L
+    dw 0b0000011001100000   ; O
+    dw 0b0000001101100000   ; S
+    dw 0b0000111001000000   ; T
+    dw 0b0000011000110000   ; Z
 
 ; Padding.
-times 510 - ($ - $$)            db 0
+;times 510 - ($ - $$)            db 0
 
 BIOS_signature:
     dw 0xAA55
 
 ; Pad to floppy disk.
-times (1440 * 1024) - ($ - $$)  db 0
+;times (1440 * 1024) - ($ - $$)  db 0
